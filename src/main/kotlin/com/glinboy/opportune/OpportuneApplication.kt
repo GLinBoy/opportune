a8b7d9d689c3019271941af45a90f9d2ebc1b180
GLinBoy <glinboy@live.com>
2025-08-17 11:08:05 +0200

    👌 IMPROVE: add application startup logging with environment details

--- src/main/kotlin/com/glinboy/opportune/OpportuneApplication.kt
+++ src/main/kotlin/com/glinboy/opportune/OpportuneApplication.kt
@@ -1,11 +1,59 @@
 package com.glinboy.opportune
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
+import java.net.InetAddress
+import java.net.UnknownHostException
 
 @SpringBootApplication
-class OpportuneApplication
+class OpportuneApplication {
 
-fun main(args: Array<String>) {
-	runApplication<OpportuneApplication>(*args)
+	private val log = LoggerFactory.getLogger(javaClass)
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			val env = runApplication<OpportuneApplication>(*args).environment
+			logApplicationStartup(env)
+		}
+
+		@JvmStatic
+		fun logApplicationStartup(env: Environment) {
+			val log = LoggerFactory.getLogger(OpportuneApplication::class.java)
+
+			var protocol = "http"
+			if (env.getProperty("server.ssl.key-store") != null) {
+				protocol = "https"
+			}
+			val serverPort = env.getProperty("server.port")
+			var contextPath = env.getProperty("server.servlet.context-path")
+			if (contextPath.isNullOrBlank()) {
+				contextPath = "/"
+			}
+			var hostAddress = "localhost"
+			try {
+				hostAddress = InetAddress.getLocalHost().hostAddress
+			} catch (e: UnknownHostException) {
+				log.warn("The host name could not be determined, using `localhost` as fallback")
+			}
+			log.info(
+				"\n----------------------------------------------------------\n\t" +
+					"Application '{}' is running! Access URLs:\n\t" +
+					"Local: \t\t{}://localhost:{}{}\n\t" +
+					"External: \t{}://{}:{}{}\n\t" +
+					"Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				protocol,
+				serverPort,
+				contextPath,
+				protocol,
+				hostAddress,
+				serverPort,
+				contextPath,
+				env.activeProfiles
+			)
+		}
+	}
 }
