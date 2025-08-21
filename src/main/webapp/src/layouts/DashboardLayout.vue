92b25b03bb6b65be6a384eca2d0d5ec6a205b20d
GLinBoy <glinboy@live.com>
2025-08-21 18:27:14 +0200

    📦 NEW: Add DashboardLayout component with basic structure

---
+++ src/main/webapp/src/layouts/DashboardLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-layout class="rounded rounded-md border">
+    <v-app-bar title="Application bar"></v-app-bar>
+    <v-main class="d-flex align-center justify-center" height="300">
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
