b3e2373be56689b22a246c1ece607ecc71a954c2
GLinBoy <glinboy@live.com>
2025-08-24 12:52:22 +0200

    📦 NEW: Add ProfileMenu, ProfileMenuAuthenticated, and ProfileMenuUnauthenticated components for user authentication handling

---
+++ src/main/webapp/src/components/profile-menu/ProfileMenuUnauthenticated.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn icon>
+    <v-avatar
+      image="https://www.gravatar.com/avatar/00000000000000000000000000000000?s=32&d=mp&f=y"
+      size="32"
+    >
+    </v-avatar>
+    <v-tooltip activator="parent" location="bottom">Login</v-tooltip>
+  </v-btn>
+</template>
+
+<script lang="ts" setup>
+</script>
