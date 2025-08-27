62eb570eb842c229abe16e51b0ebc8d2bdaa95de
GLinBoy <glinboy@live.com>
2025-08-27 19:42:38 +0200

    📦 NEW: add routing for applications and companies, including detail views and 404 handling

--- src/main/webapp/src/router/index.ts
+++ src/main/webapp/src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import BlankLayout from '@/layouts/BlankLayout.vue'
 
 import DashboardView from '@/views/DashboardView.vue'
+import ApplicationList from '@/views/application/ApplicationListView.vue';
+import ApplicationDetail from '@/views/application/ApplicationDetailView.vue';
+import CompanyList from '@/views/company/CompanyListView.vue';
+import CompanyDetail from '@/views/company/CompanyDetailView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +28,56 @@ const router = createRouter({
           component: DashboardView,
         },
       ],
+    },
+    {
+      path: '/applications',
+      name: 'applications',
+      component: DefaultLayout,
+      children: [
+        {
+          path: '',
+          name: 'applications-list',
+          component: ApplicationList,
+        },
+        {
+          path: ':id',
+          name: 'applications-detail',
+          component: ApplicationDetail,
+        },
+      ],
+    },
+    {
+      path: '/companies',
+      name: 'companies',
+      component: DefaultLayout,
+      children: [
+        {
+          path: '',
+          name: 'companies-list',
+          component: CompanyList,
+        },
+        {
+          path: ':id',
+          name: 'companies-detail',
+          component: CompanyDetail,
+        },
+      ],
+    },
+    {
+      path: '/404',
+      name: 'not-found',
+      component: BlankLayout,
+      children: [
+        {
+          path: '',
+          name: 'not-found-page',
+          component: NotFoundView,
+        },
+      ],
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/404',
     },
   ],
 })
