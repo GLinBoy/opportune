3d6133132ee6b26512e9f142d72c74acadd4ccf9
GLinBoy <glinboy@live.com>
2025-08-27 19:40:23 +0200

    📦 NEW: Add BlankLayout component for error pages

---
+++ src/main/webapp/src/layouts/BlankLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout class="rounded rounded-md border">
+    <v-main>
+      <v-container class="mx-auto" style="max-width: 1400px">
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+// Blank layout without AppBar for error pages and other minimal content
+</script>
