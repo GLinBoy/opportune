3ebf38dfd3a48b2f5d24a64b97b977713dcbe761
GLinBoy <glinboy@live.com>
2025-08-27 19:38:55 +0200

    👌 IMPROVE: Replace v-app-bar with AppBar component in DefaultLayout

--- src/main/webapp/src/layouts/DefaultLayout.vue
+++ src/main/webapp/src/layouts/DefaultLayout.vue
@@ -1,30 +1,6 @@
 <template>
   <v-layout class="rounded rounded-md border">
-    <v-app-bar flat color="primary">
-      <v-container class="mx-auto d-flex align-center justify-center" style="max-width: 1400px">
-        <v-btn icon to="/dashboard">
-          <img src="@/assets/logo.png" alt="Opportune" width="32px" height="32px" />
-        </v-btn>
-
-        <v-btn v-for="link in links" :key="link" :text="link" variant="text" class="ms-1"></v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-text-field
-          flat
-          clearable
-          hide-details
-          single-line
-          max-width="100%"
-          rounded="lg"
-          label="Search..."
-          density="compact"
-          variant="solo-filled"
-          prepend-inner-icon="mdi-magnify"
-        ></v-text-field>
-        <ProfileMenu></ProfileMenu>
-      </v-container>
-    </v-app-bar>
+    <AppBar />
 
     <v-main>
       <v-container class="mx-auto" style="max-width: 1400px">
@@ -35,6 +11,5 @@
 </template>
 
 <script setup lang="ts">
-import ProfileMenu from '@/components/profile-menu/ProfileMenu.vue'
-const links = ['Dashboard', 'Applications', 'Companies']
+import AppBar from '@/components/app-bar/AppBar.vue'
 </script>
