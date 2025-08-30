0296b6e6440e60200493787943e8e2dac9e674fe
GLinBoy <glinboy@live.com>
2025-08-30 10:11:14 +0200

    📦 NEW: add ApplicationDTO and CompanyDTO data classes

---
+++ src/main/kotlin/com/glinboy/opportune/service/dto/ApplicationDTO.kt
@@ -0,0 +1,14 @@
+package com.glinboy.opportune.service.dto
+
+data class ApplicationDTO(
+	val id: Long,
+	val jobId: Long,
+	val applicantName: String,
+	val applicantEmail: String
+)
+
+
+
+
+
+
---
+++ src/main/kotlin/com/glinboy/opportune/service/dto/CompanyDTO.kt
@@ -0,0 +1,7 @@
+package com.glinboy.opportune.service.dto
+
+data class CompanyDTO(
+	val id: Long,
+	val name: String,
+	val address: String
+)
