049b4a7774b8e1b653e135ea60ceb885990c5488
GLinBoy <glinboy@live.com>
2025-09-05 18:41:47 +0200

    📦 NEW: add AttachmentType enum for various attachment types

---
+++ src/main/kotlin/com/glinboy/opportune/enums/AttachmentType.kt
@@ -0,0 +1,12 @@
+package com.glinboy.opportune.enums
+
+enum class AttachmentType {
+    APPLICATION_RESUME,
+    COMPANY,
+    INTERVIEW,
+    RESUME,
+    COVER_LETTER,
+    PORTFOLIO,
+    CERTIFICATE,
+    OTHER
+}
