b32b370fc4dd775485ed3bfae308018d717a94ad
GLinBoy <glinboy@live.com>
2025-09-06 15:03:06 +0200

    📦 NEW: add Profile and ProfileResume repository interfaces

---
+++ src/main/kotlin/com/glinboy/opportune/repository/ProfileResumeRepository.kt
@@ -0,0 +1,10 @@
+package com.glinboy.opportune.repository
+
+import com.glinboy.opportune.entity.ProfileResume
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface ProfileResumeRepository : JpaRepository<ProfileResume, UUID>
+
