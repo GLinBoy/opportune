aa16bf985dd36084f36c6351a3b2564fd07d244c
GLinBoy <glinboy@live.com>
2025-09-07 14:06:44 +0200

    👌 IMPROVE: refactor CompanyServiceImpl to use CompanyMapper for DTO and entity mapping

--- src/main/kotlin/com/glinboy/opportune/service/impl/CompanyServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/CompanyServiceImpl.kt
@@ -3,13 +3,13 @@ package com.glinboy.opportune.service.impl
 import com.glinboy.opportune.service.CompanyService
 import com.glinboy.opportune.dto.CompanyDTO
 import com.glinboy.opportune.entity.Company
+import com.glinboy.opportune.mapper.CompanyMapper
 import com.glinboy.opportune.repository.CompanyRepository
-import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
-class CompanyServiceImpl(companyRepository: CompanyRepository, mapper: ModelMapper)
-	: GenericServiceImpl<CompanyDTO, Company, UUID, CompanyRepository>(companyRepository,
-	mapper, CompanyDTO::class.java, Company::class.java), CompanyService {
+class CompanyServiceImpl(companyRepository: CompanyRepository, mapper: CompanyMapper)
+	: GenericServiceImpl<CompanyDTO, Company, UUID, CompanyRepository, CompanyMapper>(companyRepository, mapper),
+	CompanyService {
 }
