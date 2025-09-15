040ed472fc6ca9c600de8d1f858214a3921909fa
GLinBoy <glinboy@live.com>
2025-09-15 17:04:34 +0200

    👌 IMPROVE: Add CORS configuration for development environment

---
+++ src/main/kotlin/com/glinboy/opportune/config/CorsConfiguration.kt
@@ -0,0 +1,19 @@
+package com.glinboy.opportune.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@Profile("dev")
+class CorsConfiguration : WebMvcConfigurer {
+
+  override fun addCorsMappings(registry: CorsRegistry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080", "http://localhost:9000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+  }
+}
