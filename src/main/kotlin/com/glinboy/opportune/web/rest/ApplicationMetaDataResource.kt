f642443cf7b5180e9356be209834e9693fe06f11
GLinBoy <glinboy@live.com>
2025-09-19 19:12:07 +0200

    👌 IMPROVE: Enhance ApplicationMetaDataResource with CRUD operations, validation, and pagination support

--- src/main/kotlin/com/glinboy/opportune/web/rest/ApplicationMetaDataResource.kt
+++ src/main/kotlin/com/glinboy/opportune/web/rest/ApplicationMetaDataResource.kt
@@ -2,13 +2,89 @@ package com.glinboy.opportune.web.rest
 
 import com.glinboy.opportune.dto.ApplicationMetaDataDTO
 import com.glinboy.opportune.service.ApplicationMetaDataService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.glinboy.opportune.util.PaginationUtil
+import io.swagger.v3.oas.annotations.Parameter
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.Valid
+import org.springdoc.core.converters.models.PageableAsQueryParam
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
+import java.net.URI
 import java.util.*
 
 @RestController
-@RequestMapping("/api/application-metadata")
-class ApplicationMetaDataResource(applicationMetaDataService: ApplicationMetaDataService) :
-	GenericResource<UUID, ApplicationMetaDataDTO, ApplicationMetaDataService>(applicationMetaDataService) {
-}
+@RequestMapping("/api/applications")
+class ApplicationMetaDataResource(private val applicationMetaDataService: ApplicationMetaDataService) {
 
+	@PageableAsQueryParam
+	@GetMapping("/{application_id}/metadata")
+	fun getApplicationMetaData(
+		@PathVariable("application_id") applicationId: UUID,
+		@Parameter(hidden = true) pageable: Pageable,
+		request: HttpServletRequest
+	): ResponseEntity<List<ApplicationMetaDataDTO>> {
+		val page: Page<ApplicationMetaDataDTO> = applicationMetaDataService.findAll(applicationId, pageable)
+		val headers: HttpHeaders = PaginationUtil.generatePaginationHttpHeaders(page, request.requestURI)
+		headers.accessControlExposeHeaders = listOf(HttpHeaders.LINK, "X-Total-Count")
+		return ResponseEntity(page.content, headers, HttpStatus.OK)
+	}
+
+	@GetMapping("/{application_id}/metadata/{id}")
+	fun getApplicationMetaData(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable(name = "id") id: UUID
+	): ResponseEntity<ApplicationMetaDataDTO> = applicationMetaDataService.findById(applicationId, id)
+		.map { ResponseEntity.ok().body(it) }
+		.orElse(ResponseEntity.notFound().build())
+
+	@PostMapping("/{application_id}/metadata")
+	fun saveApplicationMetaData(
+		@PathVariable("application_id") applicationId: UUID,
+		@Valid @RequestBody applicationMetaDataDTO: ApplicationMetaDataDTO,
+		request: HttpServletRequest
+	): ResponseEntity<ApplicationMetaDataDTO> {
+		if (applicationId != (applicationMetaDataDTO.applicationId)) {
+			throw ResponseStatusException(
+				HttpStatus.BAD_REQUEST, "Application ID in path must match application ID in body"
+			)
+		}
+		val savedApplicationMetaDataDTO = applicationMetaDataService.save(applicationMetaDataDTO)
+		val location = URI.create("${request.requestURI}/${savedApplicationMetaDataDTO.id}")
+		return ResponseEntity.created(location)
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(savedApplicationMetaDataDTO)
+	}
+
+	@PutMapping("/{application_id}/metadata")
+	fun updateApplicationMetaData(
+		@PathVariable("application_id") applicationId: UUID,
+		@Valid @RequestBody applicationMetaDataDTO: ApplicationMetaDataDTO
+	): ResponseEntity<ApplicationMetaDataDTO> {
+		if (applicationMetaDataDTO.id == null) {
+			throw ResponseStatusException(
+				HttpStatus.BAD_REQUEST, "ID must not be null"
+			)
+		}
+		if (applicationId != applicationMetaDataDTO.applicationId) {
+			throw ResponseStatusException(
+				HttpStatus.BAD_REQUEST, "Application ID in path must match application ID in body"
+			)
+		}
+		return ResponseEntity.ok().body(applicationMetaDataService.update(applicationMetaDataDTO))
+	}
+
+	@DeleteMapping("/{application_id}/metadata/{id}")
+	fun deleteApplicationMetaData(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable(name = "id") id: UUID
+	): ResponseEntity<Unit> {
+		applicationMetaDataService.delete(applicationId, id)
+		return ResponseEntity.noContent().build()
+	}
+}
