d37e078aa7ef0d96d2e8a950f28036f2673be525
GLinBoy <glinboy@live.com>
2025-09-20 12:58:58 +0200

    👌 IMPROVE: Implement getCompanyApplications method in ApplicationService for paginated retrieval of applications by company ID

--- src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationServiceImpl.kt
@@ -5,6 +5,8 @@ import com.glinboy.opportune.entity.Application
 import com.glinboy.opportune.mapper.ApplicationMapper
 import com.glinboy.opportune.repository.ApplicationRepository
 import com.glinboy.opportune.service.ApplicationService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -12,4 +14,9 @@ import java.util.*
 class ApplicationServiceImpl(applicationRepository: ApplicationRepository, mapper: ApplicationMapper) :
 	GenericServiceImpl<UUID, ApplicationDTO, Application, ApplicationRepository,
 		ApplicationMapper>(applicationRepository, mapper), ApplicationService {
+
+	override fun getCompanyApplications(companyId: UUID, pageable: Pageable): Page<ApplicationDTO> =
+		repository.findAllByCompanyId(companyId, pageable).let {
+			return it.map(mapper::toDto)
+		}
 }
