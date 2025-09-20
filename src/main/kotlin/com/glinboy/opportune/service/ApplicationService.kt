a81c929a5540a11385d03dc0354cb7b76e756143
GLinBoy <glinboy@live.com>
2025-09-20 12:58:49 +0200

    👌 IMPROVE: Add getCompanyApplications method to ApplicationService for retrieving paginated applications by company ID

--- src/main/kotlin/com/glinboy/opportune/service/ApplicationService.kt
+++ src/main/kotlin/com/glinboy/opportune/service/ApplicationService.kt
@@ -1,7 +1,10 @@
 package com.glinboy.opportune.service
 
 import com.glinboy.opportune.dto.ApplicationDTO
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import java.util.*
 
 interface ApplicationService : GenericService<UUID, ApplicationDTO> {
+	fun getCompanyApplications(companyId: UUID, pageable: Pageable): Page<ApplicationDTO>
 }
