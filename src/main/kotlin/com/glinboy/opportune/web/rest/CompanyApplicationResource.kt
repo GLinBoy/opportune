475518d480d4c32128d75e69e7b570ac1947bb50
GLinBoy <glinboy@live.com>
2025-09-20 12:58:38 +0200

    👌 IMPROVE: Refactor CompanyApplicationResource to correct class annotation and enhance endpoint structure

--- src/main/kotlin/com/glinboy/opportune/web/rest/CompanyApplicationResource.kt
+++ src/main/kotlin/com/glinboy/opportune/web/rest/CompanyApplicationResource.kt
@@ -13,11 +13,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
-@RestController("/api/companies")
-class CompanyApplication(private val applicationService: ApplicationService) {
+@RestController
+@RequestMapping("/api/companies")
+class CompanyApplicationResource(private val applicationService: ApplicationService) {
 
 	@PageableAsQueryParam
 	@GetMapping("/{company_id}/applications")
