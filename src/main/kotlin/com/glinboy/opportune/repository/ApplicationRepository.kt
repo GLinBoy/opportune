c2c3f2d3b585ab32e25bee574950fd62251f30af
GLinBoy <glinboy@live.com>
2025-09-20 12:58:15 +0200

    👌 IMPROVE: Add pagination support to ApplicationRepository for retrieving applications by company ID

--- src/main/kotlin/com/glinboy/opportune/repository/ApplicationRepository.kt
+++ src/main/kotlin/com/glinboy/opportune/repository/ApplicationRepository.kt
@@ -1,11 +1,15 @@
 package com.glinboy.opportune.repository
 
 import com.glinboy.opportune.entity.Application
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface ApplicationRepository : JpaRepository<Application, UUID>, JpaSpecificationExecutor<Application>
+interface ApplicationRepository : JpaRepository<Application, UUID>, JpaSpecificationExecutor<Application> {
+	fun findAllByCompanyId(companyId: UUID, pageable: Pageable): Page<Application>
+}
 
