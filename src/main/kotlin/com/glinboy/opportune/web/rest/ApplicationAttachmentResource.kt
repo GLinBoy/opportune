ed4a0a3f125c01566b474aaecbcab68d9f28c4b3
GLinBoy <glinboy@live.com>
2025-09-20 13:00:04 +0200

    👌 IMPROVE: Refactor ApplicationAttachmentResource and InterviewAttachmentResource to add CRUD operations and pagination support for attachments

--- src/main/kotlin/com/glinboy/opportune/web/rest/ApplicationAttachmentResource.kt
+++ src/main/kotlin/com/glinboy/opportune/web/rest/ApplicationAttachmentResource.kt
@@ -2,13 +2,64 @@ package com.glinboy.opportune.web.rest
 
 import com.glinboy.opportune.dto.ApplicationAttachmentDTO
 import com.glinboy.opportune.service.ApplicationAttachmentService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.glinboy.opportune.util.PaginationUtil
+import io.swagger.v3.oas.annotations.Parameter
+import jakarta.servlet.http.HttpServletRequest
+import org.springdoc.core.converters.models.PageableAsQueryParam
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
+// FIXME this should handle multiple attachments per application
 @RestController
-@RequestMapping("/api/application-attachments")
-class ApplicationAttachmentResource(applicationAttachmentService: ApplicationAttachmentService) :
-	GenericResource<UUID, ApplicationAttachmentDTO, ApplicationAttachmentService>(applicationAttachmentService) {
-}
+@RequestMapping("/api/applications")
+class ApplicationAttachmentResource(private val applicationAttachmentService: ApplicationAttachmentService) {
 
+	@PageableAsQueryParam
+	@GetMapping("/{application_id}/attachments")
+	fun getInterviewAttachments(
+		@PathVariable("application_id") applicationId: UUID,
+		@Parameter(hidden = true) pageable: Pageable,
+		request: HttpServletRequest
+	): ResponseEntity<List<ApplicationAttachmentDTO>> {
+		val page = applicationAttachmentService.findByApplicationId(applicationId, pageable)
+		val headers: HttpHeaders =
+			PaginationUtil.generatePaginationHttpHeaders(page, request.requestURI)
+		headers.accessControlExposeHeaders = listOf(HttpHeaders.LINK, "X-Total-Count")
+		return ResponseEntity(page.content, headers, HttpStatus.OK)
+	}
+
+	@GetMapping("/{application_id}/attachments/{id}")
+	fun getInterviewAttachment(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable(name = "id") id: UUID
+	): ResponseEntity<ApplicationAttachmentDTO> =
+		applicationAttachmentService.findByApplicationIdAndId(applicationId, id)
+			.map { ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(it) }
+			.orElse(ResponseEntity.notFound().build())
+
+	@DeleteMapping("/{application_id}/attachments/{id}")
+	fun deleteInterviewAttachment(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable(name = "id") id: UUID
+	): ResponseEntity<Void> {
+		applicationAttachmentService.deleteByApplicationIdAndId(applicationId, id)
+		return ResponseEntity.noContent().build<Void>()
+	}
+
+	@PostMapping("/{application_id}/attachments", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+	fun uploadInterviewAttachment(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable("note_id") noteId: UUID,
+		@RequestParam("interview_attachment") interviewAttachment: MultipartFile,
+		request: HttpServletRequest
+	): ResponseEntity<ApplicationAttachmentDTO> {
+		// TODO implement file upload (FileService?)
+		return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
+	}
+}
--- src/main/kotlin/com/glinboy/opportune/web/rest/InterviewAttachmentResource.kt
+++ src/main/kotlin/com/glinboy/opportune/web/rest/InterviewAttachmentResource.kt
@@ -2,13 +2,66 @@ package com.glinboy.opportune.web.rest
 
 import com.glinboy.opportune.dto.InterviewAttachmentDTO
 import com.glinboy.opportune.service.InterviewAttachmentService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.glinboy.opportune.util.PaginationUtil
+import io.swagger.v3.oas.annotations.Parameter
+import jakarta.servlet.http.HttpServletRequest
+import org.springdoc.core.converters.models.PageableAsQueryParam
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
 @RestController
-@RequestMapping("/api/interview-attachments")
-class InterviewAttachmentResource(interviewAttachmentService: InterviewAttachmentService) :
-	GenericResource<UUID, InterviewAttachmentDTO, InterviewAttachmentService>(interviewAttachmentService) {
-}
+@RequestMapping("/api/applications")
+class InterviewAttachmentResource(private val interviewAttachmentService: InterviewAttachmentService) {
 
+	@PageableAsQueryParam
+	@GetMapping("/{application_id}/interview-notes/{interview_note_id}/attachments")
+	fun getInterviewAttachments(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable("interview_note_id") interviewNoteId: UUID,
+		@Parameter(hidden = true) pageable: Pageable,
+		request: HttpServletRequest
+	): ResponseEntity<List<InterviewAttachmentDTO>> {
+		val page = interviewAttachmentService.findByApplicationIdAndInterviewNoteId(applicationId, interviewNoteId, pageable)
+		val headers: HttpHeaders =
+			PaginationUtil.generatePaginationHttpHeaders(page, request.requestURI)
+		headers.accessControlExposeHeaders = listOf(HttpHeaders.LINK, "X-Total-Count")
+		return ResponseEntity(page.content, headers, HttpStatus.OK)
+	}
+
+	@GetMapping("/{application_id}/interview-notes/{interview_note_id}/attachments/{id}")
+	fun getInterviewAttachment(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable("interview_note_id") interviewNoteId: UUID,
+		@PathVariable(name = "id") id: UUID
+	): ResponseEntity<InterviewAttachmentDTO> =
+		interviewAttachmentService.findByApplicationIdANdInterviewNoteIdAndId(applicationId, interviewNoteId, id)
+			.map { ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(it) }
+			.orElse(ResponseEntity.notFound().build())
+
+	@DeleteMapping("/{application_id}/interview-notes/{interview_note_id}/attachments/{id}")
+	fun deleteInterviewAttachment(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable("interview_note_id") interviewNoteId: UUID,
+		@PathVariable(name = "id") id: UUID
+	): ResponseEntity<Void> {
+		interviewAttachmentService.deleteByApplicationIdAndInterviewNoteIdAndId(applicationId, interviewNoteId, id)
+		return ResponseEntity.noContent().build<Void>()
+	}
+
+	@PostMapping("/{application_id}/interview-notes/{interview_note_id}/attachments", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+	fun uploadInterviewAttachment(
+		@PathVariable("application_id") applicationId: UUID,
+		@PathVariable("interview_note_id") noteId: UUID,
+		@RequestParam("interview_attachment") interviewAttachment: MultipartFile,
+		request: HttpServletRequest
+	): ResponseEntity<InterviewAttachmentDTO> {
+		// TODO implement file upload (FileService?)
+		return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
+	}
+}
