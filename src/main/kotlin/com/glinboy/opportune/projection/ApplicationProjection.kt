b9a77f2c455056ec243af2c05bc776a7009ad93b
GLinBoy <glinboy@live.com>
2025-10-09 13:23:45 +0200

    📦 NEW: Add ApplicationProjection interface for application data representation

---
+++ src/main/kotlin/com/glinboy/opportune/projection/ApplicationProjection.kt
@@ -0,0 +1,18 @@
+package com.glinboy.opportune.projection
+
+import com.glinboy.opportune.enums.ApplicationStatus
+import java.time.Instant
+import java.util.*
+
+interface ApplicationProjection {
+    fun getId(): UUID?
+    fun getUrl(): String?
+    fun getTitle(): String?
+    fun getLocation(): String?
+    fun getAppliedAt(): Instant?
+    fun getSalary(): String?
+    fun getNote(): String?
+    fun getStatus(): ApplicationStatus?
+    fun getCompanyId(): UUID?
+    fun getCompanyName(): String?
+}
