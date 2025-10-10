31fe59b6566590871565dd7061847e2b6beef5a5
GLinBoy <glinboy@live.com>
2025-10-10 16:21:29 +0200

    📦 NEW: Add custom queries to ApplicationRepository for enhanced application data retrieval

--- src/main/kotlin/com/glinboy/opportune/repository/ApplicationRepository.kt
+++ src/main/kotlin/com/glinboy/opportune/repository/ApplicationRepository.kt
@@ -1,15 +1,40 @@
 package com.glinboy.opportune.repository
 
 import com.glinboy.opportune.entity.Application
+import com.glinboy.opportune.projection.ApplicationProjection
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface ApplicationRepository : JpaRepository<Application, UUID>, JpaSpecificationExecutor<Application> {
 	fun findAllByCompanyId(companyId: UUID, pageable: Pageable): Page<Application>
-}
 
+	@Query("SELECT a.id AS id, " +
+		" a.url AS url, " +
+		" a.title AS title, " +
+		" a.location AS location, " +
+		" a.appliedAt AS appliedAt, " +
+		" a.salary AS salary, " +
+		" a.note AS note, " +
+		" a.status AS status, " +
+		" a.company.id AS companyId, " +
+		" c.name AS companyName " +
+		" FROM Application a " +
+		" LEFT JOIN a.company c")
+	fun findAllApplications(pageable: Pageable): Page<ApplicationProjection>
+
+	@Query("SELECT a FROM Application a " +
+		" LEFT JOIN FETCH a.company " +
+		" LEFT JOIN FETCH a.timeline " +
+		" LEFT JOIN FETCH a.interviewNotes " +
+		" LEFT JOIN FETCH a.metadata " +
+		" LEFT JOIN FETCH a.resume " +
+		" LEFT JOIN FETCH a.attachments " +
+		" WHERE a.id = :id")
+	fun findApplicationDetailsById(id: UUID): Optional<Application>
+}
