73cbeffa2e66c35b9f35a1cfbddd5b490d06df10
GLinBoy <glinboy@live.com>
2025-10-10 16:21:39 +0200

    📦 NEW: Enhance ApplicationService with methods for retrieving application details and projections

--- src/main/kotlin/com/glinboy/opportune/service/ApplicationService.kt
+++ src/main/kotlin/com/glinboy/opportune/service/ApplicationService.kt
@@ -1,10 +1,14 @@
 package com.glinboy.opportune.service
 
 import com.glinboy.opportune.dto.ApplicationDTO
+import com.glinboy.opportune.dto.ApplicationDetailsDTO
+import com.glinboy.opportune.projection.ApplicationProjection
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import java.util.*
 
 interface ApplicationService : GenericService<UUID, ApplicationDTO> {
 	fun getCompanyApplications(companyId: UUID, pageable: Pageable): Page<ApplicationDTO>
+	fun findAllApplications(pageable: Pageable): Page<ApplicationProjection>
+	fun getApplicationDetails(id: UUID): Optional<ApplicationDetailsDTO>
 }
--- src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationServiceImpl.kt
@@ -1,8 +1,11 @@
 package com.glinboy.opportune.service.impl
 
 import com.glinboy.opportune.dto.ApplicationDTO
+import com.glinboy.opportune.dto.ApplicationDetailsDTO
 import com.glinboy.opportune.entity.Application
+import com.glinboy.opportune.mapper.ApplicationDetailsMapper
 import com.glinboy.opportune.mapper.ApplicationMapper
+import com.glinboy.opportune.projection.ApplicationProjection
 import com.glinboy.opportune.repository.ApplicationRepository
 import com.glinboy.opportune.service.ApplicationService
 import org.springframework.data.domain.Page
@@ -11,12 +14,21 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class ApplicationServiceImpl(applicationRepository: ApplicationRepository, mapper: ApplicationMapper) :
-	GenericServiceImpl<UUID, ApplicationDTO, Application, ApplicationRepository,
-		ApplicationMapper>(applicationRepository, mapper), ApplicationService {
+class ApplicationServiceImpl(
+	applicationRepository: ApplicationRepository,
+	mapper: ApplicationMapper,
+	private val applicationDetailsMapper: ApplicationDetailsMapper
+) : GenericServiceImpl<UUID, ApplicationDTO, Application, ApplicationRepository,
+	ApplicationMapper>(applicationRepository, mapper), ApplicationService {
 
 	override fun getCompanyApplications(companyId: UUID, pageable: Pageable): Page<ApplicationDTO> =
 		repository.findAllByCompanyId(companyId, pageable).let {
 			return it.map(mapper::toDto)
 		}
+
+	override fun findAllApplications(pageable: Pageable): Page<ApplicationProjection> =
+		repository.findAllApplications(pageable)
+
+	override fun getApplicationDetails(id: UUID): Optional<ApplicationDetailsDTO> = repository.findApplicationDetailsById(id)
+		.map { applicationDetailsMapper.toDto(it) }
 }
