506704041d7f856cf2c1116ddc85cde12627d7a1
GLinBoy <glinboy@live.com>
2025-10-15 10:54:36 +0200

    📦 UPDATE: Refactor CompanyMetaDataServiceImpl and CompanyMetaDataService to extend GenericChildService for parent-child entity operations

--- src/main/kotlin/com/glinboy/opportune/service/CompanyMetaDataService.kt
+++ src/main/kotlin/com/glinboy/opportune/service/CompanyMetaDataService.kt
@@ -1,13 +1,7 @@
 package com.glinboy.opportune.service
 
 import com.glinboy.opportune.dto.CompanyMetaDataDTO
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import java.util.*
 
-interface CompanyMetaDataService : GenericService<UUID, CompanyMetaDataDTO> {
-	fun findAll(companyId: UUID, pageable: Pageable): Page<CompanyMetaDataDTO>
-	fun findById(companyId: UUID, id: UUID): Optional<CompanyMetaDataDTO>
-	fun delete(companyId: UUID, id: UUID): Unit
-}
+interface CompanyMetaDataService : GenericChildService<UUID, CompanyMetaDataDTO>
 
--- src/main/kotlin/com/glinboy/opportune/service/impl/CompanyMetaDataServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/CompanyMetaDataServiceImpl.kt
@@ -5,33 +5,14 @@ import com.glinboy.opportune.entity.CompanyMetaData
 import com.glinboy.opportune.mapper.CompanyMetaDataMapper
 import com.glinboy.opportune.repository.CompanyMetaDataRepository
 import com.glinboy.opportune.service.CompanyMetaDataService
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
 class CompanyMetaDataServiceImpl(companyMetaDataRepository: CompanyMetaDataRepository, mapper: CompanyMetaDataMapper) :
-	GenericServiceImpl<UUID, CompanyMetaData, CompanyMetaDataDTO, CompanyMetaDataRepository,
+	GenericChildServiceImpl<UUID, CompanyMetaData, CompanyMetaDataDTO, CompanyMetaDataRepository,
 		CompanyMetaDataMapper>(companyMetaDataRepository, mapper), CompanyMetaDataService {
 
-	override fun findAll(companyId: UUID, pageable: Pageable): Page<CompanyMetaDataDTO> =
-		repository.findAllByCompanyId(companyId, pageable)
-			.let {
-			return it.map(mapper::toDto)
-		}
-
-	override fun findById(companyId: UUID, id: UUID): Optional<CompanyMetaDataDTO> =
-		repository.findOneByCompanyIdAndId(companyId, id)
-			.let {
-				return it.map(mapper::toDto)
-			}
-
-	@Transactional
-	override fun delete(companyId: UUID, id: UUID) {
-		repository.deleteByCompanyIdAndId(companyId, id)
-	}
-
+	override fun getParentFieldName(): String = "company"
 }
 
