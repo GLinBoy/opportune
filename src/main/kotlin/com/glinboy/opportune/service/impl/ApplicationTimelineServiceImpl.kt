31b2cbde8c9f8b9e733c713bbe7a88def2ffb863
GLinBoy <glinboy@live.com>
2025-10-15 10:54:26 +0200

    📦 UPDATE: Refactor ApplicationTimelineServiceImpl and ApplicationTimelineService to extend GenericChildService for parent-child entity operations

--- src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationTimelineServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationTimelineServiceImpl.kt
@@ -5,34 +5,15 @@ import com.glinboy.opportune.entity.ApplicationTimeline
 import com.glinboy.opportune.mapper.ApplicationTimelineMapper
 import com.glinboy.opportune.repository.ApplicationTimelineRepository
 import com.glinboy.opportune.service.ApplicationTimelineService
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
 class ApplicationTimelineServiceImpl(
 	applicationTimelineRepository: ApplicationTimelineRepository,
 	mapper: ApplicationTimelineMapper
-) : GenericServiceImpl<UUID, ApplicationTimeline, ApplicationTimelineDTO, ApplicationTimelineRepository,
+) : GenericChildServiceImpl<UUID, ApplicationTimeline, ApplicationTimelineDTO, ApplicationTimelineRepository,
 	ApplicationTimelineMapper>(applicationTimelineRepository, mapper), ApplicationTimelineService {
 
-	override fun findAll(applicationId: UUID, pageable: Pageable): Page<ApplicationTimelineDTO> =
-		repository.findAllByApplicationId(applicationId, pageable)
-			.let {
-			return it.map(mapper::toDto)
-		}
-
-	override fun findById(applicationId: UUID, id: UUID): Optional<ApplicationTimelineDTO> =
-		repository.findOneByApplicationIdAndId(applicationId, id)
-			.let {
-			return it.map(mapper::toDto)
-		}
-
-	@Transactional
-	override fun delete(applicationId: UUID, id: UUID) {
-		repository.deleteByApplicationIdAndId(applicationId, id)
-	}
-
+	override fun getParentFieldName(): String = "application"
 }
