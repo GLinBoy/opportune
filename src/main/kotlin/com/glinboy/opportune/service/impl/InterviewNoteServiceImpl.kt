b33cade26a25e11d480d71543e1eca885f60a442
GLinBoy <glinboy@live.com>
2025-10-15 10:54:47 +0200

    📦 UPDATE: Refactor InterviewNoteServiceImpl and InterviewNoteService to extend GenericChildService for parent-child entity operations

--- src/main/kotlin/com/glinboy/opportune/service/impl/InterviewNoteServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/InterviewNoteServiceImpl.kt
@@ -5,32 +5,13 @@ import com.glinboy.opportune.entity.InterviewNote
 import com.glinboy.opportune.mapper.InterviewNoteMapper
 import com.glinboy.opportune.repository.InterviewNoteRepository
 import com.glinboy.opportune.service.InterviewNoteService
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
 class InterviewNoteServiceImpl(interviewNoteRepository: InterviewNoteRepository, mapper: InterviewNoteMapper) :
-	GenericServiceImpl<UUID, InterviewNote, InterviewNoteDTO, InterviewNoteRepository,
+	GenericChildServiceImpl<UUID, InterviewNote, InterviewNoteDTO, InterviewNoteRepository,
 		InterviewNoteMapper>(interviewNoteRepository, mapper), InterviewNoteService {
 
-	override fun findAll(applicationId: UUID, pageable: Pageable): Page<InterviewNoteDTO> =
-		repository.findAllByApplicationId(applicationId, pageable)
-			.let {
-			return it.map(mapper::toDto)
-		}
-
-	override fun findById(applicationId: UUID, id: UUID): Optional<InterviewNoteDTO> =
-		repository.findOneByApplicationIdAndId(applicationId, id)
-			.let {
-			return it.map(mapper::toDto)
-		}
-
-	@Transactional
-	override fun delete(applicationId: UUID, id: UUID) {
-		repository.deleteByApplicationIdAndId(applicationId, id)
-	}
-
+	override fun getParentFieldName(): String = "application"
 }
