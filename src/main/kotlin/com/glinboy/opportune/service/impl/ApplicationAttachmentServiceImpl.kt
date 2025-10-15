dcf07f6dc9da86c0d28f2381b734da595451e8b5
GLinBoy <glinboy@live.com>
2025-10-15 10:54:05 +0200

    📦 UPDATE: Refactor ApplicationAttachmentService to extend GenericChildService for improved parent-child entity handling

--- src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationAttachmentServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/ApplicationAttachmentServiceImpl.kt
@@ -5,32 +5,15 @@ import com.glinboy.opportune.entity.ApplicationAttachment
 import com.glinboy.opportune.mapper.ApplicationAttachmentMapper
 import com.glinboy.opportune.repository.ApplicationAttachmentRepository
 import com.glinboy.opportune.service.ApplicationAttachmentService
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Service
 class ApplicationAttachmentServiceImpl(
 	applicationAttachmentRepository: ApplicationAttachmentRepository,
 	mapper: ApplicationAttachmentMapper
-) : GenericServiceImpl<UUID, ApplicationAttachment, ApplicationAttachmentDTO, ApplicationAttachmentRepository,
+) : GenericChildServiceImpl<UUID, ApplicationAttachment, ApplicationAttachmentDTO, ApplicationAttachmentRepository,
 	ApplicationAttachmentMapper>(applicationAttachmentRepository, mapper), ApplicationAttachmentService {
 
-	override fun findByApplicationIdAndId(applicationId: UUID, id: UUID): Optional<ApplicationAttachmentDTO> =
-		repository.findByApplicationIdAndId(applicationId, id)
-			.map(mapper::toDto)
-			.let { return it }
-
-	override fun findByApplicationId(applicationId: UUID, pageable: Pageable): Page<ApplicationAttachmentDTO> =
-		repository.findAllByApplicationId(applicationId, pageable)
-			.map(mapper::toDto)
-			.let { return it }
-
-	@Transactional
-	override fun deleteByApplicationIdAndId(applicationId: UUID, id: UUID) =
-		repository.deleteByApplicationIdAndId(applicationId, id)
-
-
+	override fun getParentFieldName(): String = "application"
 }
