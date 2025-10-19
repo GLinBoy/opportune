5badb9384eb6988257c2fc18daed86c82be8cde0
GLinBoy <glinboy@live.com>
2025-10-19 16:12:00 +0200

    👌 IMPROVE: Add Bearer authentication security scheme to OpenAPI configuration

--- src/main/kotlin/com/glinboy/opportune/config/OpenApiConfiguration.kt
+++ src/main/kotlin/com/glinboy/opportune/config/OpenApiConfiguration.kt
@@ -1,15 +1,20 @@
 package com.glinboy.opportune.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class OpenApiConfiguration {
+	companion object {
+		const val BEARER_AUTHENTICATION_NAME = "Bearer Authentication"
+	}
 
 	@Bean
 	fun customizeOpenAPI(properties: ApplicationProperties): OpenAPI = OpenAPI()
@@ -30,4 +35,15 @@ class OpenApiConfiguration {
 				.url(properties.info.website)
 		)
 		.servers(properties.info.servers?.map { Server().url(it.url).description(it.description) } )
+		.components(
+			Components()
+				.addSecuritySchemes(
+					BEARER_AUTHENTICATION_NAME,
+					SecurityScheme()
+						.name(BEARER_AUTHENTICATION_NAME)
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("Bearer")
+						.bearerFormat("JWT")
+				)
+		)
 }
