d314da9ac3e9fe55cc046fd26fd22645ec930ade
GLinBoy <glinboy@live.com>
2025-10-26 10:12:26 +0100

    📦 NEW: Add methods for password reset code creation and management in VerificationCodeService

--- src/main/kotlin/com/glinboy/opportune/service/VerificationCodeService.kt
+++ src/main/kotlin/com/glinboy/opportune/service/VerificationCodeService.kt
@@ -4,8 +4,11 @@ import com.glinboy.opportune.dto.VerificationCodeDTO
 import java.util.*
 
 interface VerificationCodeService : GenericService<UUID, VerificationCodeDTO> {
-	fun createEmailVerificationKey(id: UUID): VerificationCodeDTO?
+	fun createEmailVerificationCode(id: UUID): VerificationCodeDTO?
 	fun findByEmailVerification(id: UUID): Optional<VerificationCodeDTO>
 	fun deleteAllProfileEmailVerification(profileId: UUID)
+	fun createPasswordResetCode(id: UUID): VerificationCodeDTO?
+	fun findByPasswordReset(id: UUID): Optional<VerificationCodeDTO>
+	fun deleteAllProfilePasswordReset(profileId: UUID)
 }
 
--- src/main/kotlin/com/glinboy/opportune/service/impl/VerificationCodeServiceImpl.kt
+++ src/main/kotlin/com/glinboy/opportune/service/impl/VerificationCodeServiceImpl.kt
@@ -16,7 +16,7 @@ class VerificationCodeServiceImpl(verificationCodeRepository: VerificationCodeRe
 	GenericServiceImpl<UUID, VerificationCode, VerificationCodeDTO, VerificationCodeRepository, VerificationCodeMapper>(verificationCodeRepository, mapper),
 	VerificationCodeService {
 
-	override fun createEmailVerificationKey(id: UUID): VerificationCodeDTO? {
+	override fun createEmailVerificationCode(id: UUID): VerificationCodeDTO? {
 		val verificationCode = VerificationCode(
 			type = VerificationCodeType.EMAIL_VERIFICATION,
 			profile = Profile(id = id)
@@ -32,6 +32,24 @@ class VerificationCodeServiceImpl(verificationCodeRepository: VerificationCodeRe
 	@Transactional
 	override fun deleteAllProfileEmailVerification(profileId: UUID) =
 		repository.deleteAllByProfileIdAndType(profileId, VerificationCodeType.EMAIL_VERIFICATION)
+
+	override fun createPasswordResetCode(id: UUID): VerificationCodeDTO? {
+		val verificationCode = VerificationCode(
+			type = VerificationCodeType.PASSWORD_RESET,
+			profile = Profile(id = id)
+		)
+		return mapper.toDto(repository.save(verificationCode))
+	}
+
+	override fun findByPasswordReset(id: UUID): Optional<VerificationCodeDTO> =
+		repository
+			.findByIdAndType(id, VerificationCodeType.PASSWORD_RESET)
+			.map(mapper::toDto)
+
+	@Transactional
+	override fun deleteAllProfilePasswordReset(profileId: UUID) {
+		repository.deleteAllByProfileIdAndType(profileId, VerificationCodeType.PASSWORD_RESET)
+	}
 
 }
 
