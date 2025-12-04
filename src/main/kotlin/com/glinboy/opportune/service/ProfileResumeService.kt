889e6d340acfcdcb24c5aab10b7da649d0216792
GLinBoy <glinboy@live.com>
2025-12-04 18:48:11 +0100

    👌 IMPROVE: Enhance ProfileResumeService with file management methods for user profiles

--- src/main/kotlin/com/glinboy/opportune/service/ProfileResumeService.kt
+++ src/main/kotlin/com/glinboy/opportune/service/ProfileResumeService.kt
@@ -1,12 +1,17 @@
 package com.glinboy.opportune.service
 
 import com.glinboy.opportune.dto.ProfileResumeDTO
+import org.springframework.core.io.Resource
+import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
 interface ProfileResumeService : GenericService<UUID, ProfileResumeDTO> {
+	fun saveFile(profileResumeFile: MultipartFile): ProfileResumeDTO
 	fun findByProfileId(profileId: UUID): Optional<ProfileResumeDTO>
 	fun deleteByProfileId(profileId: UUID): Unit
-	fun findByProfileIdForCurrentUser(profileId: UUID): Optional<ProfileResumeDTO>
-	fun deleteByProfileIdForCurrentUser(profileId: UUID): Unit
+	fun findForCurrentUser(): Optional<ProfileResumeDTO>
+	fun deleteForCurrentUser(): Unit
+	fun getFileResourceForCurrentUser(id: UUID): Pair<Resource, ProfileResumeDTO>
+	fun getFileResourceForCurrentUser(): Pair<Resource, ProfileResumeDTO>
 }
 
