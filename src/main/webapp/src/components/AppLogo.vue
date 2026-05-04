4c0530b8ff579348eb40a76884000d0caa3c1b60
GLinBoy <glinboy@live.com>
2026-05-04 12:10:51 +0200

    📦 NEW: Add AppLogo component with template and styling

---
+++ src/main/webapp/src/components/AppLogo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="d-flex align-center justify-center my-4">
+    <img src="@/assets/logo.png" alt="Opportune" width="48" height="48" class="mr-2" />
+    <span class="text-title-large font-weight-bold">Opportune</span>
+  </div>
+</template>
