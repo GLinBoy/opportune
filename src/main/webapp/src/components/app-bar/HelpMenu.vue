4a1fd6c67defd239d45faa873bdd8b3f2dc1c995
GLinBoy <glinboy@live.com>
2026-05-06 14:48:25 +0200

    📦 NEW: Add HelpMenu component for user assistance with links to documentation, support, and keyboard shortcuts

---
+++ src/main/webapp/src/components/app-bar/HelpMenu.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-menu v-model="helpOpen" :close-on-content-click="true" offset="8" max-width="220">
+    <template #activator="{ props: menuProps }">
+      <v-btn v-bind="menuProps" variant="text" size="small" icon aria-label="Help">
+        <v-icon>mdi-help-circle-outline</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list density="compact" nav>
+      <v-list-subheader>Help &amp; Resources</v-list-subheader>
+
+      <v-list-item
+        prepend-icon="mdi-book-open-outline"
+        title="Documentation"
+        href="/docs"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+      <v-list-item
+        prepend-icon="mdi-lifebuoy"
+        title="Support"
+        href="/support"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+      <v-list-item
+        prepend-icon="mdi-keyboard-outline"
+        title="Keyboard shortcuts"
+        @click="showShortcuts"
+      />
+      <v-divider class="my-1" />
+      <v-list-item
+        prepend-icon="mdi-information-outline"
+        title="What's new"
+        href="/changelog"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+    </v-list>
+  </v-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const helpOpen = ref(false)
+
+function showShortcuts() {
+  // TODO: open keyboard shortcuts dialog
+}
+</script>
