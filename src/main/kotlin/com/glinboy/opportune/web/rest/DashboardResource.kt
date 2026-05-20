12001eb4ed0c116b000c45df9ba6665b95659455
GLinBoy <glinboy@live.com>
2026-05-20 14:07:39 +0200

    📦 NEW: Add DashboardResource for user dashboard summary endpoint

---
+++ src/main/kotlin/com/glinboy/opportune/web/rest/DashboardResource.kt
@@ -0,0 +1,21 @@
+package com.glinboy.opportune.web.rest
+
+import com.glinboy.opportune.config.OpenApiConfiguration
+import com.glinboy.opportune.dto.UserDashboardSummaryDTO
+import com.glinboy.opportune.projection.ApplicationStatProjection
+import com.glinboy.opportune.service.DashboardService
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/dashboard")
+@SecurityRequirement(name = OpenApiConfiguration.BEARER_AUTHENTICATION_NAME)
+class DashboardResource (private val dashboardService: DashboardService) {
+
+	@GetMapping("/summery")
+	fun summery(): ResponseEntity<UserDashboardSummaryDTO> = ResponseEntity
+		.ok(dashboardService.getCurrentUserSummery())
+}
