ab5ecaab734d3b5af9ea136d0c39c2805753ab14
GLinBoy <glinboy@live.com>
2026-05-20 14:04:24 +0200

    👌 IMPROVE: Add getUserSummary method to ApplicationService for user dashboard data retrieval

--- src/main/kotlin/com/glinboy/opportune/service/ApplicationService.kt
+++ src/main/kotlin/com/glinboy/opportune/service/ApplicationService.kt
@@ -3,7 +3,9 @@ package com.glinboy.opportune.service
 import com.glinboy.opportune.dto.ApplicationDTO
 import com.glinboy.opportune.dto.ApplicationDetailsDTO
 import com.glinboy.opportune.dto.ApplicationUrlSubmissionDTO
+import com.glinboy.opportune.dto.UserDashboardSummaryDTO
 import com.glinboy.opportune.projection.ApplicationProjection
+import com.glinboy.opportune.projection.ApplicationStatProjection
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import java.util.*
@@ -16,4 +18,5 @@ interface ApplicationService : GenericService<UUID, ApplicationDTO> {
 	fun findAllApplicationsForCurrentUser(pageable: Pageable): Page<ApplicationProjection>
 	fun getApplicationDetailsForCurrentUser(id: UUID): Optional<ApplicationDetailsDTO>
 	fun submitApplicationUrl(submission: ApplicationUrlSubmissionDTO): Optional<ApplicationDTO>
+	fun getUserSummery(currentUserID: UUID): UserDashboardSummaryDTO
 }
