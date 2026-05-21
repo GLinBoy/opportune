cdff4ae57ed45ee60ef54ab8ced544957ed9581d
GLinBoy <glinboy@live.com>
2026-05-21 11:31:55 +0200

    👌 IMPROVE: Add summaryDays field to UserDashboardSummaryDTO for enhanced dashboard data

--- src/main/kotlin/com/glinboy/opportune/dto/UserDashboardSummaryDTO.kt
+++ src/main/kotlin/com/glinboy/opportune/dto/UserDashboardSummaryDTO.kt
@@ -4,6 +4,7 @@ import com.glinboy.opportune.projection.ApplicationStatProjection
 import com.glinboy.opportune.projection.ScoreSummaryProjection
 
 data class UserDashboardSummaryDTO(
+	val summaryDays: Long,
 	val stats: List<ApplicationStatProjection>,
 	val scores: ScoreSummaryProjection
 )
