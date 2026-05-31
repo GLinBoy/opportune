08213a7c7420f24fef2631afea73a53f4bcbc415
GLinBoy <glinboy@live.com>
2026-05-31 13:50:19 +0200

    👌 IMPROVE: replace @GeneratedValue with @UuidGenerator for UUID generation in BaseEntity

--- src/main/kotlin/com/glinboy/opportune/entity/BaseEntity.kt
+++ src/main/kotlin/com/glinboy/opportune/entity/BaseEntity.kt
@@ -1,16 +1,15 @@
 package com.glinboy.opportune.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.UuidGenerator
 import java.util.*
 
 @MappedSuperclass
 @JsonIgnoreProperties("hibernateLazyInitializer", "handler")
 abstract class BaseEntity {
 	@Id
-	@GeneratedValue(strategy = GenerationType.UUID)
+	@UuidGenerator(style = UuidGenerator.Style.VERSION_7)
 	open val id: UUID? = null
 }
