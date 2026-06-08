875b92effe7a509852ff47d1d89c154ea56871b6
GLinBoy <glinboy@live.com>
2026-06-08 17:08:37 +0200

    feat: redesign admin users list view

--- src/main/webapp/src/components/UserAvatar.vue
+++ src/main/webapp/src/components/UserAvatar.vue
@@ -7,7 +7,7 @@ import { computed } from 'vue'
 import CryptoJS from 'crypto-js'
 
 const props = defineProps<{
-  email: string
+  email?: string
   avatarUrl?: string
   size?: number
 }>()
@@ -16,8 +16,9 @@ const displayUrl = computed(() => {
   if (props.avatarUrl) {
     return props.avatarUrl
   }
-  const trimmedEmail = props.email.trim().toLowerCase()
-  const hash = CryptoJS.MD5(trimmedEmail).toString()
+  const email = (props.email ?? '').trim().toLowerCase()
+  if (!email) return ''
+  const hash = CryptoJS.MD5(email).toString()
   const s = props.size ?? 40
   return `https://www.gravatar.com/avatar/${hash}?s=${s}&d=mp&r=g`
 })
