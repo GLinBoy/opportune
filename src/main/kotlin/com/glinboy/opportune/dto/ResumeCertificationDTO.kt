3af707e0e760fea353f34dccfda9c6a1a5df5a5e
GLinBoy <glinboy@live.com>
2026-07-01 04:01:06 +0200

    👌 IMPROVE: Update ResumeCertification and ResumeCertificationDTO to use month/year fields for issue and expiration dates

--- src/main/kotlin/com/glinboy/opportune/dto/ResumeCertificationDTO.kt
+++ src/main/kotlin/com/glinboy/opportune/dto/ResumeCertificationDTO.kt
@@ -1,7 +1,6 @@
 package com.glinboy.opportune.dto
 
 import java.time.Instant
-import java.time.LocalDate
 import java.util.*
 
 data class ResumeCertificationDTO(
@@ -10,8 +9,10 @@ data class ResumeCertificationDTO(
 	override val lastModifiedDate: Instant? = null,
 	val name: String? = null,
 	val issuingOrganization: String? = null,
-	val issueDate: LocalDate? = null,
-	val expirationDate: LocalDate? = null,
+	val issueMonth: Short? = null,
+	val issueYear: Short? = null,
+	val expirationMonth: Short? = null,
+	val expirationYear: Short? = null,
 	val credentialId: String? = null,
 	val credentialUrl: String? = null,
 	val displayOrder: Int = 0,
